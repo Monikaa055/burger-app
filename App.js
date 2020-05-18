2733b8f753e097c7038a7920c62ba49de174fc1a
Monikaa055 <49594551+Monikaa055@users.noreply.github.com>
2020-05-18 21:07:12 +0530

    added burger app files

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+function App() {
+  return (
+    <div>
+      <Layout>Test</Layout>
+      <BurgerBuilder />
+    </div>
+  );
+}
+
+export default App;
